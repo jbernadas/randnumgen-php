6ad7ec723692776a948370072f8b22fde9172da8
jb <joseph.bernadas.ctr@tri.global>
2022-04-22 12:32:55 -0700

    Adds Server side button

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <style>
     #num-box-wrapper {
-        height: 100vh;
         font-size: 4rem;
     }
     .num-box {
@@ -19,7 +18,7 @@
         border-radius: 20px;
         margin-top: 35vh;
     }
-    #num-box-wrapper .btn.btn-primary {
+    #num-box-wrapper .btn {
         padding: 10px 65px;
         border-radius: 10px;
     }
@@ -31,7 +30,14 @@
             <div class="col-md-6 col-sm-8 col-xs-12 offset-md-3 offset-sm-2">
                 <div id="num-box-wrapper" class="text-center">
                     <div class="num-box">Hello World!</div>
-                    <div class="btn btn-primary">Click Me!</div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="btn btn-primary">Client Side!</div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="btn btn-success">Server Side!</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
