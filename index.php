7b40453b224e4889f6017606030d09fc92a16132
jb <joseph.bernadas.ctr@tri.global>
2022-04-22 13:42:52 -0700

    Adds random numbers via client side script

--- index.php
+++ index.php
@@ -8,19 +8,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <style>
+    body {
+        background: #fff2f2;
+    }
     #num-box-wrapper {
         font-size: 4rem;
     }
     .num-box {
         padding: 2px 5px 5px;
-        border: 6px solid #ff9b9b;
+        border: 6px inset #ff9b9b;
         line-height: 4rem;
         border-radius: 20px;
         margin-top: 35vh;
+        color: #6a6a6a;
+        overflow-wrap: anywhere;
+        background: #d2ebff;
     }
     #num-box-wrapper .btn {
         padding: 10px 65px;
-        border-radius: 10px;
+        border-radius: 7px;
     }
 </style>
 </head>
@@ -29,10 +35,10 @@
         <div class="row">
             <div class="col-md-6 col-sm-8 col-xs-12 offset-md-3 offset-sm-2">
                 <div id="num-box-wrapper" class="text-center">
-                    <div class="num-box">Hello World!</div>
+                    <div class="num-box" id="numbBox">Hello World!</div>
                     <div class="row">
                         <div class="col-md-6">
-                            <div class="btn btn-primary">Client Side!</div>
+                            <div class="btn btn-primary" onclick="numbBoxUpdater()">Client Side!</div>
                         </div>
                         <div class="col-md-6">
                             <div class="btn btn-success">Server Side!</div>
@@ -42,5 +48,13 @@
             </div>
         </div>
     </div>
+    <script>
+        function randNumGen() {
+            return Math.floor(Math.random() * 1000000000000) * 1;
+        }
+        function numbBoxUpdater() {
+            document.getElementById('numbBox').textContent = randNumGen()
+        }
+    </script>
 </body>
 </html>
