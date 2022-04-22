35020a39bceee1c29ed2467cd414a32aa3ead8c4
jb <joseph.bernadas.ctr@tri.global>
2022-04-22 15:40:13 -0700

    Adds php code

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+
+<?php
+    function php_rand_num() {
+        $rnumb = '';
+        $rnumb = rand();
+        echo $rnumb;
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,10 +15,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Random Number Generator</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <style>
     body {
         background: #fff2f2;
+    }
+    h1.text-center {
+        margin-top: 2vw;
+        color: grey;
+        font-size: 4rem;
     }
     #num-box-wrapper {
         font-size: 4rem;
@@ -19,7 +32,7 @@
         border: 6px inset #ff9b9b;
         line-height: 4rem;
         border-radius: 20px;
-        margin-top: 35vh;
+        margin-top: 30vh;
         color: #6a6a6a;
         overflow-wrap: anywhere;
         background: #d2ebff;
@@ -33,15 +46,16 @@
 <body>
     <div class="container">
         <div class="row">
+            <h1 class="text-center">Random Number Generator</h1>
             <div class="col-md-6 col-sm-8 col-xs-12 offset-md-3 offset-sm-2">
                 <div id="num-box-wrapper" class="text-center">
                     <div class="num-box" id="numbBox">Hello World!</div>
                     <div class="row">
                         <div class="col-md-6">
-                            <div class="btn btn-primary" onclick="numbBoxUpdater()">Client Side!</div>
+                            <div class="btn btn-primary" onclick="clientSideNumbBoxUpdater()">Client Side!</div>
                         </div>
                         <div class="col-md-6">
-                            <div class="btn btn-success">Server Side!</div>
+                            <div class="btn btn-success" onclick="serverSideNumbBoxUpdater()">Server Side!</div>
                         </div>
                     </div>
                 </div>
@@ -50,10 +64,14 @@
     </div>
     <script>
         function randNumGen() {
-            return Math.floor(Math.random() * 1000000000000) * 1;
+            return Math.floor(Math.random() * 100000000000000) * 1;
         }
-        function numbBoxUpdater() {
+        function clientSideNumbBoxUpdater() {
             document.getElementById('numbBox').textContent = randNumGen()
+        }
+        function serverSideNumbBoxUpdater() {
+            document.getElementById('numbBox').textContent = "<?php php_rand_num(); ?>";
+
         }
     </script>
 </body>
